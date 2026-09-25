81e919b73cb9cf6f8a07ab55e4fcd61ddb151c85
businessio087-hue <businessio087@gmail.com>
2026-09-25 10:30:12 +0000

    last touches

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     if (window.AOS) {
+        const revealTargets = document.querySelectorAll(
+            'section > div:not([data-aos]), .glass:not([data-aos]), footer:not([data-aos])'
+        );
+
+        revealTargets.forEach((element, index) => {
+            element.dataset.aos = 'fade-up';
+            element.dataset.aosDelay = String((index % 4) * 70);
+        });
+
         AOS.init({
             once: true,
             duration: 650,
